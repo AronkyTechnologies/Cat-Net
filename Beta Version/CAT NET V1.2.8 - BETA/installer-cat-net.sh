5ca10f646d426922458b663aa9c4e6e0335546c8
IsTk0 - Anto <97669353+IsTk0@users.noreply.github.com>
2022-06-03 11:47:36 +0200

    Add files via upload

---
+++ Beta Version/CAT NET V1.2.8 - BETA/installer-cat-net.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Created by IsTk0
+
+echo "Created by IsTk0 For Geeks Peaple"
+echo "$(tput setaf 3) ██████╗ █████╗ ████████╗   ███╗   ██╗███████╗████████╗    ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     ███████╗██████╗ "
+echo "$(tput setaf 3)██╔════╝██╔══██╗╚══██╔══╝   ████╗  ██║██╔════╝╚══██╔══╝    ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     ██╔════╝██╔══██╗"
+echo "$(tput setaf 3)██║     ███████║   ██║█████╗██╔██╗ ██║█████╗     ██║       ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     █████╗  ██████╔╝"
+echo "$(tput setaf 3)██║     ██╔══██║   ██║╚════╝██║╚██╗██║██╔══╝     ██║       ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     ██╔══╝  ██╔══██╗"
+echo "$(tput setaf 3)╚██████╗██║  ██║   ██║      ██║ ╚████║███████╗   ██║       ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗███████╗██║  ██║"
+echo "$(tput setaf 3) ╚═════╝╚═╝  ╚═╝   ╚═╝      ╚═╝  ╚═══╝╚══════╝   ╚═╝       ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝"
+
+sudo pip install sys
+sudo pip install os
+sudo pip install paramiko
+sudo pip install time
+sudo pip install subprocess
+sudo pip install ping3
+sudo pip install scapy.all
+sudo pip install scapy
+sudo pip install argparse
+
+sudo sh ./launcher.sh
