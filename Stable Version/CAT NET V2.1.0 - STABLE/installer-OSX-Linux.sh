baaecb6f7a772f84d3b1f4f7b23b4d8a66291d79
IsTk0 - Anto <97669353+IsTk0@users.noreply.github.com>
2022-06-05 02:00:15 +0200

    Add files via upload

---
+++ Stable Version/CAT NET V2.1.0 - STABLE/installer-OSX-Linux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#Created by IsTk0
+
+echo "Created by IsTk0 For Geeks Peaple"
+echo "$(tput setaf 3) ██████╗ █████╗ ████████╗   ███╗   ██╗███████╗████████╗    ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     ███████╗██████╗ "
+echo "$(tput setaf 3)██╔════╝██╔══██╗╚══██╔══╝   ████╗  ██║██╔════╝╚══██╔══╝    ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     ██╔════╝██╔══██╗"
+echo "$(tput setaf 3)██║     ███████║   ██║█████╗██╔██╗ ██║█████╗     ██║       ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     █████╗  ██████╔╝"
+echo "$(tput setaf 3)██║     ██╔══██║   ██║╚════╝██║╚██╗██║██╔══╝     ██║       ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     ██╔══╝  ██╔══██╗"
+echo "$(tput setaf 3)╚██████╗██║  ██║   ██║      ██║ ╚████║███████╗   ██║       ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗███████╗██║  ██║"
+echo "$(tput setaf 3) ╚═════╝╚═╝  ╚═╝   ╚═╝      ╚═╝  ╚═══╝╚══════╝   ╚═╝       ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝"
+
+sudo pip install xmlrpc
+sudo pip install colorama
+sudo pip install subprocess
+sudo pip install sys
+sudo pip install os
+sudo pip install time 
+sudo pip install paramiko
+sudo pip install ping3
+sudo pip install scapy.all
+sudo pip install argparse
+sudo pip install getpass 
+sudo pip install ftplib
+sudo pip install socket
+sudo pip installplatform
+sudo pip install psutil
+
+git clone https://github.com/AronkyTechnologies/Cat-Net.git
+
+cd "Stable Version"
+cd "CAT NET V2.0.0 - STABLE"
+cd "OSX-Linux"
+
+sudo sh ./Cat-Net-Launcher.sh
