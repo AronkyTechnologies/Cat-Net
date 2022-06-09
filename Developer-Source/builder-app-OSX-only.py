0c693966f78863c1a6588fc92e8cc9eeef7b2c77
IsTk0 - Anto <97669353+IsTk0@users.noreply.github.com>
2022-06-10 01:26:33 +0200

    Add files via upload

---
+++ Developer-Source/builder-app-OSX-only.py
@@ -0,0 +1,16 @@
+#THIS IS A INTERNAL FILE OF ARONKY, DONT DIVULGATE IT !
+from setuptools import setup
+
+#before running the code read the attached readme. and after use: "python3 builder-app-OSX-only.py" py2app
+
+APP = ["****"] #name of main code of cat-net
+OPTIONS = {
+    'iconfile':'cat-net-icon.icns', #icon file. FOR ONLY MAC USE .icns FILE NOT .ico
+    'argv_emulation': True,
+}
+
+setup(
+    app=APP,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app']
+)
